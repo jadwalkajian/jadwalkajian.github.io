447126ce4fe8e4083cbd0733a8bdad6388c99014
dickyerfan <dickyerfan@gmail.com>
2021-12-23 14:05:26 +0700

    Merubah background audio menjadi transparent

--- asset/mishaari.js
+++ asset/mishaari.js
@@ -3,7 +3,7 @@ $.getJSON('https://raw.githubusercontent.com/penggguna/QuranJSON/master/quran.js
     $.each(data, function(i, data){
         $('#daftar_menu').append(`
         <div class="col-md-6 col-lg-4 mb-1">
-            <li class="nama_surat card ps-4 fs-4 shadow">
+            <li class="nama_surat card ps-4 fs-4 shadow bg-transparent">
             ${data.number_of_surah}. ${data.name} / ${data.name_translations.ar}
                 <audio controls>
                     <source src="${data.recitation}" type="audio/mpeg">
